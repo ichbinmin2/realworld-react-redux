b843e9324943d1bf7b6e57719a1eb565a572cdc5
JiyeonMin <ichbinmin2@gmail.com>
2022-02-10 20:44:36 +0900

    ✨ Be ready to start to todo MVC

--- todoMVC-react-ts/webpack.config.js
+++ todoMVC-react-ts/webpack.config.js
@@ -2,18 +2,19 @@ const path = require("path");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: "./src/index.jsx", // 파일을 찾기 시작하는 입구에요.
+  entry: "./src/index.jsx", // 파일을 찾기 시작하는 입구
   mode: "development",
   resolve: {
-    extensions: [".js", ".jsx"],
-    // import from 뒤에 확장자를 생략했을 때, 어떤 확장자가 올 수 있는지 알려줘요.
+    extensions: [".js", ".jsx", ".ts", ".tsx"],
+    // import from 뒤에 확장자를 생략했을 때, 어떤 확장자가 올 수 있는지 알려줌
+    modules: [path.join(__dirname, "src"), "node_modules"], // 절대경로 설정
   },
   module: {
     rules: [
       {
         test: /.(js|jsx|ts|tsx)$/,
         exclude: /node_modules/,
-        use: "babel-loader", // js와 jsx 파일을 가져오는 loader에요.
+        use: "babel-loader", // js와 jsx 파일을 가져오는 loader
       },
       {
         test: /.css$/,
