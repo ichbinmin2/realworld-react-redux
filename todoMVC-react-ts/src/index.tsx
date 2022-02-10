c6706c8f572cf54c4da3ecd772f78fc1561fd87c
JiyeonMin <ichbinmin2@gmail.com>
2022-02-10 20:33:49 +0900

    🌱  Update fundamental files

--- todoMVC-react-ts/src/index.tsx
+++ todoMVC-react-ts/src/index.tsx
@@ -1,10 +1,5 @@
-import ReactDOM from "react-dom";
 import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
 
-function App() {
-  const name = "TaeHee";
-
-  return <h1>Hello {name}</h1>;
-}
-
-ReactDOM.render(<App />, document.getElementById("app"));
+ReactDOM.render(<App />, document.querySelector("#app"));
