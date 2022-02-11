75b01d6c8f65690c975911d488748501b8c49c79
JiyeonMin <ichbinmin2@gmail.com>
2022-02-11 14:53:28 +0900

    ✨ Add task status detection handler to Header

--- todoMVC-react-ts/src/components/Header.tsx
+++ todoMVC-react-ts/src/components/Header.tsx
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Header = () => {
+  const [task, setTask] = useState("");
+
+  const onInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(event.target.value);
+    setTask(event.target.value);
+  };
+
   return (
     <header className="header">
       <h1>todos</h1>
       <input
         className="new-todo"
         placeholder="What needs to be done?"
-        value=""
+        value={task}
+        onChange={onInputChange}
       />
     </header>
   );
