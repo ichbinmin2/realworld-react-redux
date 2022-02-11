be7dd0a0c105d8d6eb9665c09b0603056cfae1f4
JiyeonMin <ichbinmin2@gmail.com>
2022-02-11 14:45:12 +0900

    📝  Set an initial state to todoList state

--- todoMVC-react-ts/src/components/Main.tsx
+++ todoMVC-react-ts/src/components/Main.tsx
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import TodoItem from "./TodoItem";
+import { v4 as uuid } from "uuid";
 
 const Main = () => {
+  const initialState = [
+    { id: uuid(), task: "To do an assignment", completed: false },
+    { id: uuid(), task: "Be a FE developer", completed: false },
+    { id: uuid(), task: "Do what I do like", completed: false },
+  ];
+
+  const [todoList, setTodoList] = useState(initialState);
+  // initialState 배열 값을 todoList default 값으로 담음.
+
   return (
     <section className="main">
       <input
@@ -12,7 +22,9 @@ const Main = () => {
       />
       <label htmlFor="toggle-all"></label>
       <ul className="todo-list">
-        <TodoItem task="To do list" completed={false} />
+        {todoList.map((todo) => {
+          <TodoItem key={todo.id} id={todo.id} task={todo.task} completed={todo.completed} />;
+        })}
       </ul>
     </section>
   );
--- todoMVC-react-ts/src/components/TodoItem.tsx
+++ todoMVC-react-ts/src/components/TodoItem.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 
 type Todo = {
+  id: string;
   task: string;
   completed: boolean;
 };
 
-const TodoItem = ({ task, completed }: Todo) => {
+const TodoItem = ({ id, task, completed }: Todo) => {
   return (
-    <li className={completed ? "completed" : ""}>
+    <li className={completed ? "completed" : ""} data-task-id={id}>
+      {/* Q. data-task-id={id} 속성을 받아오는 이유 */}
       <div className="view">
         <input className="toggle" type="checkbox" checked={completed} />
         <label>{task}</label>
