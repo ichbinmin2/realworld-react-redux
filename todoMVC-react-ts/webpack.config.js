18b93f1ed3f4a4674b3faf15d0b816980b565e8a
JiyeonMin <ichbinmin2@gmail.com>
2022-02-11 12:56:26 +0900

    🔧  Update webpack config file

--- todoMVC-react-ts/webpack.config.js
+++ todoMVC-react-ts/webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: "./src/index.jsx", // 파일을 찾기 시작하는 입구
+  entry: "./src/index.tsx", // 파일을 찾기 시작하는 입구
   mode: "development",
   resolve: {
     extensions: [".js", ".jsx", ".ts", ".tsx"],
